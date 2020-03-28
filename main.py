0b3a187052edd74cf874583fd4ed512f72aa44db
JoaquinGomez1 <joaquingomez3098@gmail.com>
2020-03-28 13:13:41 -0300

    Solucionados bugs de duplicacion de carpetas

--- main.py
+++ main.py
@@ -4,45 +4,62 @@ from os import listdir
 import time
 import shutil
 
+"""IMPORTANTE: El programa no está modularizado para simplificar la implementación."""
 
 def RemoveDuplicates(listaDup):
 	listaSinDup = list(dict.fromkeys(listaDup))
 	return listaSinDup
 
-MainDir = sys.argv[1]
+try:
+	MainDir = sys.argv[1]	# El formato en cmd es: 'python <nombrescript> <direccion> <parametro>'
 
-if sys.argv [2] == '--check':
-	if os.path.exists(MainDir):
-		print('Si existe')
+	if len(str(sys.argv)) == 3:  # Con este bloque verifico si ingresó un parametro adicional
+		if sys.argv [2] == '--check':
+			if os.path.exists(MainDir):
+				print('Si existe')
+			else:
+				print('no existe')
+		else:
+			print(f'Has ingresado un parámetro inexistente ({sys.argv[2]})')
+
+
+	elif os.path.exists(str(MainDir)):
+		listOfFiles = [file for file in listdir(MainDir)] # Contiene todos los archivos
+		listOfTypes = []  # Contiene todas las extensiones de cada archivo
+
+		print(f'\nSe han encontrado los siguientes archivos: {listOfFiles}')
+
+		for archivo in listOfFiles:
+			if '.' in archivo: # Si el nombre tiene un punto considero que lo que procede es su extensión.
+				if archivo == 'desktop.ini': # El escritorio tiene este archivo que lo inicializa al bootear.
+					continue
+
+				auxVar = archivo.split('.')
+				listOfTypes.append(auxVar[1])
+
+		listOfTypes = RemoveDuplicates(listOfTypes)
+
+		for tipo in range(len(listOfTypes)):	
+			NewDir = (MainDir + f'/{listOfTypes[tipo]}')
+
+			if os.path.exists(NewDir) == False: # Controlo que la carpeta no exista previamente
+				os.mkdir(NewDir) # para que pueda ser creada sin errores
+
+		for i in range(len(listOfFiles)):  # Muevo a cada carpeta el archivo que corresponda
+			for z in range(len(listOfTypes)):
+				if listOfFiles[i].endswith(listOfTypes[z]):
+					shutil.move(MainDir + f'/{listOfFiles[i]}', MainDir + f'/{listOfTypes[z]}')
+
+		time.sleep(0.5)
+		print('...')	# Si ya llego a este punto sin errores, los archivos se organizaron
+		time.sleep(1)	#  de manera correcta. Con esto simulamos para el usuario un último análisis.
+		print('Todos los archivos han sido organizados correctamente.')
+
+
 	else:
-		print('no existe')
+		print(f'El directorio:  "{MainDir}" no existe')
+		print('Si tu carpeta tiene espacios por favor escribe\
+		 toda la direccion entre doble comilla (" ")')
 
-
-elif os.path.exists(str(MainDir)):
-	listOfFiles = [file for file in listdir(MainDir)]
-	listOfTypes = []
-	print(listOfFiles)
-
-	for archivo in listOfFiles:
-		if '.' in archivo: # Si el nombre tiene un punto considero que lo que procede es su extensión.
-			if archivo == 'desktop.ini': # El escritorio tiene este archivo que lo inicializa al bootear.
-				continue
-
-			auxVar = archivo.split('.')
-			listOfTypes.append(auxVar[1])
-
-
-	listOfTypes = RemoveDuplicates(listOfTypes)
-
-	for tipo in range(len(listOfTypes)):
-		NewDir = MainDir + f'/{listOfTypes[tipo]}'
-		os.mkdir(NewDir)
-	
-
-	for i in range(len(listOfFiles)):
-		for z in range(len(listOfTypes)):
-			if listOfFiles[i].endswith(listOfTypes[z]):
-				shutil.move(MainDir + f'/{listOfFiles[i]}', MainDir + f'/{listOfTypes[z]}')
-
-else:
-	print(f'El directorio:  "{MainDir}" no existe')
+except IndexError:
+	print('Ha ocurrido un error con la cantidad de parámetros y no se ha realizado ningún cambio.')
